b92b6e9230bef35b743d8e9fbff6890d0cc6626f
chasergit <89519450+chasergit@users.noreply.github.com>
2025-09-01 19:53:23 +0300

    Delete js directory

--- js/models/grass_long.js
+++
@@ -1,3 +0,0 @@
-OBJLoader.load("./models/grass_long.obj",function(object){
-mesh["grass_long"]=object.children[0];
-});
--- js/models/models_list.js
+++
@@ -1,3 +0,0 @@
-let OBJLoader=new THREE_OBJLoader.OBJLoader(loadingManager);
-OBJLoader.mat=mat;
-let FBXLoader=new THREE_FBXLoader.FBXLoader(loadingManager);
